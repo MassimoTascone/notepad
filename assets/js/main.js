b78acd84262ea2ed3575130b2539be222c8d22e1
Massimo Tascone <tascone.massimo@gmail.com>
2019-09-30 16:23:57 +0200

    color + font + size

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,20 @@
 
-let changeFont;
-
-
-(() => {
-
-    let changeFont = function(font){
+    changeFont = function(font){ // fonction qui change font family a partir de value
         console.log(font.value);
         document.getElementById("txt").style.fontFamily = font.value;
     }
 
+    changeSize = function(size){ // fonction qui change font size apd value
+        console.log(size.value);
+        document.getElementById("txt").style.fontSize = size.value;
+    }
+
+
+
+    changeColor = function(color){
+        console.log(color.value);
+        document.getElementById("txt").style.color = color.value;
+    }
 
 
 
@@ -16,5 +22,3 @@ let changeFont;
     
 
 
-
-})();    
