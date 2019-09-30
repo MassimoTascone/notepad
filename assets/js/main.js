5c2a5f4e72e6d574da88e5f78c502d44d04a60fb
Massimo Tascone <tascone.massimo@gmail.com>
2019-09-30 12:27:33 +0200

    early tests

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+
+let changeFont;
+
+
+(() => {
+
+    let changeFont = function(font){
+        console.log(font.value);
+        document.getElementById("txt").style.fontFamily = font.value;
+    }
+
+
+
+
+
+    
+
+
+
+})();    
